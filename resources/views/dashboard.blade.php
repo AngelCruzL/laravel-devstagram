f17e66fdf0b90d7cf4254e21efa2a68b7c9f8faf
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-11-01 09:59:27 -0600

    feat: add profile account information on his account page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,31 @@
 @extends('layouts.app')
 
-@section('pageTitle', 'Tu Cuenta')
+@section('pageTitle', $user->username)
 
 @section('content')
   <div class="flex justify-center">
-    <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-      <div class="px-5 md:w-8/12 lg:w-6/12">
+    <div class="flex flex-col items-center w-full md:w-8/12 lg:w-6/12 md:flex-row">
+      <div class="w-8/12 px-5 lg:w-6/12">
         <img src="{{asset('img/user.svg')}}" alt="User default image">
       </div>
 
-      <div class="px-5 md:w-8/12 lg:w-6/12">
-        <p class="text-2xl text-gray-700">{{ auth()->user()->name }}</p>
+      <div class="flex flex-col items-center px-5 py-10 md:w-8/12 lg:w-6/12 md:justify-center md:items-start md:py-10">
+        <p class="text-2xl text-gray-700">{{ $user->username }}</p>
+
+        <p class="mt-5 mb-3 text-sm font-bold to-gray-800">
+          0
+          <span class="font-normal">Seguidores</span>
+        </p>
+
+        <p class="mb-3 text-sm font-bold to-gray-800">
+          0
+          <span class="font-normal">Siguiendo</span>
+        </p>
+
+        <p class="mb-3 text-sm font-bold to-gray-800">
+          0
+          <span class="font-normal">Posts</span>
+        </p>
       </div>
     </div>
   </div>
