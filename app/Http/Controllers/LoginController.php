b14d3596dd94828cdd69ecdf16cb057426cb8aec
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-03-01 17:50:26 -0600

    fix: username argument on login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,27 +1,27 @@
 <?php
 
-namespace App\Http\Controllers;
+  namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+  use Illuminate\Http\Request;
 
-class LoginController extends Controller
-{
-  public function index()
+  class LoginController extends Controller
   {
-    return view('auth.login');
-  }
-
-  public function store(Request $request)
-  {
-    $this->validate($request, [
-      'email' => 'required|email',
-      'password' => 'required',
-    ]);
-
-    if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
-      return back()->with('message', 'Credenciales incorrectas');
+    public function index()
+    {
+      return view('auth.login');
     }
 
-    return redirect()->route('post.index');
+    public function store(Request $request)
+    {
+      $this->validate($request, [
+        'email' => 'required|email',
+        'password' => 'required',
+      ]);
+
+      if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
+        return back()->with('message', 'Credenciales incorrectas');
+      }
+
+      return redirect()->route('posts.index', auth()->user()->username);
+    }
   }
-}
