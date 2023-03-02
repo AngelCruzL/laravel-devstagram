3c9372ad144aef8f69c6d8c511fe7aad4976fabf
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-03-01 18:15:44 -0600

    chore: add dropzone to handle image upload

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import { Dropzone } from 'dropzone';
+
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone('#dropzone', {
+  dictDefaultMessage: 'Arrastra la imagen aquí o haz click para subirla',
+  acceptedFiles: 'image/{jpg,png,jpeg,gif}',
+  dictInvalidFileType: 'Solo se permiten imágenes con formato jpg, png, jpeg o gif',
+  addRemoveLinks: true,
+  dictRemoveFile: 'Eliminar Imagen',
+  maxFiles: 1,
+  uploadMultiple: false
+});
