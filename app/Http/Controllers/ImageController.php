0e15dc7c282d3375131dbbedcf34d4ac97d77abf
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-22 11:34:39 -0600

    Add intervention image crop functionality on image upload

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class ImageController extends Controller
 {
   public function store(Request $request)
   {
-    $input = $request->file('file');
-    return response()->json(['image' => $input->extension()]);
+    $image = $request->file('file');
+    $image_name = Str::uuid() . '.' . $image->extension();
+    $image_path = public_path('uploads') . '/' . $image_name;
+
+    $image_server = Image::make($image);
+    $image_server->fit(1000, 1000);
+    $image_server->save($image_path);
+
+    return response()->json(['image' => $image_name]);
   }
 }
