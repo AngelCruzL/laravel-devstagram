2f75aa92d2302a64769dba7d3ace9a4d012aeec0
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-24 10:22:30 -0600

    Display likes quantity

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -58,7 +58,9 @@
           @endif
         @endauth
 
-        <p>0 Likes</p>
+        <p class='font-bold'>{{ $post->likes->count() }}
+          <span class='font-normal'>Likes</span>
+        </p>
       </div>
 
       <div>
