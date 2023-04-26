d6c9d506f497bd7bb6af506bb2bdfcb269d58cd6
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-26 07:39:41 -0600

    Add like logic inside like livewire component

--- app/Http/Livewire/PostLike.php
+++ app/Http/Livewire/PostLike.php
@@ -12,4 +12,15 @@ class PostLike extends Component
   {
     return view('livewire.post-like');
   }
+
+  public function like()
+  {
+    if ($this->post->check_like(auth()->user())) {
+      $this->post->likes()->where('post_id', $this->post->id)->delete();
+    } else {
+      $this->post->likes()->create([
+        'user_id' => auth()->user()->id,
+      ]);
+    }
+  }
 }
