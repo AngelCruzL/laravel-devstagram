fa7a808b973b6b6ff746a6f17115b4e0b91d51af
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-25 21:01:08 -0600

    Add home page functionality and protect the route only to authenticated users

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+
   public function __invoke(): View
   {
-    return view('home');
+    // Get the posts from the people the user follows
+    $ids = auth()->user()->following->pluck('id')->toArray();
+    $posts = Post::whereIn('user_id', $ids)->latest()->paginate(10);
+
+    return view('home', [
+      'posts' => $posts,
+    ]);
   }
 }
