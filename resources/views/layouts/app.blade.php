ddac66501f607edd694334cab0041b0097311020
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-25 19:22:20 -0600

    Add home page controller and route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,9 @@
 <body class="bg-gray-100">
 <header class="p-5 bg-white border-b shadow">
   <div class="container flex items-center justify-between mx-auto">
-    <h1 class="text-3xl font-black">DevStagram</h1>
+    <a href='{{ route('home') }}' class="text-3xl font-black">
+      DevStagram
+    </a>
 
     @auth
       <nav class="flex items-center gap-2">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FollowerController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ImageController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
@@ -22,9 +23,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('main-page');
-});
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('/crear-cuenta', [RegisterController::class, 'index'])->name('register');
 Route::post('/crear-cuenta', [RegisterController::class, 'store']);
